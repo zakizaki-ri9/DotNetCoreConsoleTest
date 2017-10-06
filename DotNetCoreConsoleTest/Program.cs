17e08b749af3303ca25af675a2eb9772159d50f8
zakizaki-ri9 <kic.yuuki@gmail.com>
2017-10-06 17:52:12 +0900

    コミットテスト

--- DotNetCoreConsoleTest/Program.cs
+++ DotNetCoreConsoleTest/Program.cs
@@ -7,6 +7,7 @@ namespace DotNetCoreConsoleTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Commit Test!");
         }
     }
 }
