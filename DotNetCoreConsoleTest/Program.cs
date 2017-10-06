80fdeff8d3a6da343e869a730837ca9ed7c27106
zakizaki-ri9 <kic.yuuki@gmail.com>
2017-10-06 17:46:54 +0900

    プロジェクト ファイルを追加します。

---
+++ DotNetCoreConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCoreConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
